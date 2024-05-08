9ab25c5665ace4bfb6cfa901ba5a9d54ae39cd84
Aleksandr Safonov <siaalex03@gmail.com>
2024-05-08 10:26:15 +0400

    #1

---
+++ TestIq/Data/CustomerModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace TestIq.Data;
+
+public class CustomerModel
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+    [Required]
+    [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Только кириллица")]
+    [StringLength(20, MinimumLength = 2)]
+    public string Name { get; set; } = "";
+    [Required]
+    [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Только кириллица")]
+    [StringLength(20, MinimumLength = 2)]
+    public string LastName { get; set; } = "";
+    [Required]
+    [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Только кириллица")]
+    [StringLength(25, MinimumLength = 3)]
+    public string Country { get; set; } = "";
+    [Required]
+    [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Только кириллица")]
+    [StringLength(25, MinimumLength = 3)]
+    public string City { get; set; } = "";
+    
+}
---
+++ TestIq/MongoDbServices.cs
@@ -0,0 +1,82 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using TestIq.Data;
+
+namespace TestIq
+{
+    public class MongoDbServices
+    {
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<CustomerModel> collection;
+        
+        
+        
+        public MongoDbServices()
+        {
+            try
+            {
+                client = new MongoClient("mongodb://127.0.0.1:27017");
+                database = client.GetDatabase("IQ");
+                collection = database.GetCollection<CustomerModel>("Customer");                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+        
+        public  List<CustomerModel> GetCustomers()
+        {
+            try
+            {
+                return collection.Find(FilterDefinition<CustomerModel>.Empty).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public CustomerModel GetCustomer(string studentId)
+        {
+            try
+            {
+                return collection.Find(x => x.Id == studentId).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        
+        public void SaveUpdate(CustomerModel customer) 
+        {
+            try
+            {
+                var customerobj = collection.Find(x => x.Id == customer.Id).FirstOrDefault();
+                if (customerobj == null)
+                {
+                    collection.InsertOne(customer);
+                }
+                else
+                {
+                    collection.ReplaceOne(x => x.Id == customer.Id, customer);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+        
+
+    }
+    
+}
